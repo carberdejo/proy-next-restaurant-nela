499ff0c43ba8f98607b401dc2f7ce5a0e980a754
Carlos Berdejo <carlosberdejopar@gmail.com>
2025-05-13 20:40:50 -0500

    Sets up basic project structure and UI
    Initializes the project with basic components.
    
    - Removes default Next.js assets and configurations.
    - Adds a basic layout with a header.
    - Introduces new components for categories.
    - Adds custom fonts.

---
+++ src/app/carta/page.tsx
@@ -0,0 +1,21 @@
+
+
+
+import ButtonCategorias from '@/components/ButtonCategorias';
+import Sidebar from '@/components/Sidebar';
+
+
+
+export default function Home() {
+    
+    return (
+    <main className="">
+        
+        <h1>Pagina de las cartas</h1>
+        <ButtonCategorias/>
+        <Sidebar/>
+    
+
+    </main>
+);
+}
---
+++ src/components/ButtonCategorias.tsx
@@ -0,0 +1,25 @@
+import { categorias } from '@/data/common'
+import Image from 'next/image';
+export default function ButtonCategorias() {
+  return (
+    <section className='w-full h-40 flex justify-around items-center p-15'>
+        {categorias.map((categoria) => (
+            <div key={categoria.id} className='flex flex-col items-center'>
+            <div
+            className='w-16 h-16 flex items-center justify-center bg-amber-800 rounded-full shadow-lg 
+                    transition-all duration-300 ease-in-out transform hover:scale-110 hover:bg-amber-600'
+        >
+        <Image
+            src={categoria.image}
+            alt={categoria.name}
+            width={40}
+            height={40}
+            className='object-contain'
+        />
+        </div>
+        <p className='mt-2 text-sm font-medium'>{categoria.name}</p>
+    </div>
+    ))}
+    </section>
+  )
+}
